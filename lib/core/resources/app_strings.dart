2b4a93c309a5316399bf9e741f8ef174bbc00e27
Your Name <your.email@example.com>
2025-02-05 16:59:05 +0200

    Initial commit - upload movie app project

---
+++ lib/core/resources/app_strings.dart
@@ -0,0 +1,37 @@
+class AppStrings {
+  static const String appTitle = 'CineSphere';
+  static const String seeAll = 'View All';
+  static const String popularMovies = 'Trending Movies';
+  static const String topRatedMovies = 'Highly Rated Films';
+  static const String story = 'Synopsis';
+  static const String videos = 'Clips & Trailers';
+  static const String cast = 'Actors & Crew';
+  static const String reviews = 'Audience Reviews';
+  static const String similar = 'You May Also Like';
+  static const String showLess = 'Collapse';
+  static const String showMore = 'Expand';
+  static const String movies = 'Films';
+  static const String shows = 'TV Series';
+  static const String search = 'Discover';
+  static const String watchlist = 'My Watchlist';
+  static const String popularShows = 'Trending Series';
+  static const String topRatedShows = 'Critically Acclaimed';
+  static const String lastEpisodeOnAir = 'Latest Episode Aired';
+  static const String seasons = 'All Seasons';
+  static const String season = 'Season';
+  static const String episodes = 'All Episodes';
+  static const String episode = 'Episode';
+  static const String airDate = 'Aired On:';
+  static const String lastEpisode = 'Latest Episode';
+  static const String searchText =
+      'Find the best movies and series by searching here. Get top recommendations instantly.';
+  static const String searchHint = 'Search for movies, series, actors...';
+  static const String watchlistIsEmpty = 'Your Watchlist is Empty';
+  static const String watchlistText =
+      'Save your favorite movies and series here to watch later.';
+  static const String oops = 'Oops!';
+  static const String tryAgainLater = 'Please try again in a while';
+  static const String errorMessage = 'Something went wrong. Please check back later.';
+  static const String tryAgain = 'Retry';
+  static const String noResults = 'No matches found';
+}
